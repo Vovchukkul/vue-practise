57a6395d11710fd5684329bbddba851dc1829c99
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-19 21:23:10 +0200

    add

--- vue-project/deploy.sh
+++ vue-project/deploy.sh
@@ -9,19 +9,24 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# Initialize a new Git repository
+git init
+
+# Add a .nojekyll file to avoid Jekyll processing (if needed)
 echo > .nojekyll
 
-git init
-git checkout -B main
+# Commit all files
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# Create an orphan branch named gh-pages
+git checkout --orphan gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Vovchukkul/vue-practise.git main:gh-pages
+# Commit the changes to the gh-pages branch
+git commit -m 'deploy'
 
+# Push to the "gh-pages" branch of the remote repository
+git push -f git@github.com:Vovchukkul/vue-practise.git gh-pages
+
+# Clean up
 cd -
